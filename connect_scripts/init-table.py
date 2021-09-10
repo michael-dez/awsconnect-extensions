34b8a04e29b895707b7be2246fb0f87f92c6b449
Michael Mendez <michael.dez@gmail.com>
2021-09-10 01:38:42 +0000

    removed swap

--- connect_scripts/init-table.py
+++ connect_scripts/init-table.py
@@ -3,10 +3,16 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name = 'us-east-1')
 table = dynamodb.Table('AgenttoAgent')
 
-newItem = {
-    "Extension": "0000",
+for x in range(10000):
+    extension = str(x)
+    extension = extension.zfill(4)
+        
+    newItem = {
+    "Extension": extension,
     "AgentLoginName": "",
-    "InUse": True
+    "InUse": False 
     }
-response = table.put_item(Item=newItem)
-print(response)
+    response = table.put_item(Item=newItem)
+    print(newItem)
+    print(response)
+    
