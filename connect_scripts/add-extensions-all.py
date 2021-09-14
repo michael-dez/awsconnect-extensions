5c3130250b003d6a899b6cd8440e103748bb0b23
mike <michael.dez@gmail.com>
2021-09-13 22:53:42 -0600

    fetches complete agent list

--- connect_scripts/add-extensions-all.py
+++ connect_scripts/add-extensions-all.py
@@ -1,12 +1,27 @@
 import boto3
 import json
+# TODO:function to check if agent has an extension, function to assign extension, and get_response function for exception handling and readability
 
 conClient = boto3.client('connect')
+users = []
+iid ='794790f5-0ae2-4348-806a-f58bf245ab3f'
 
 response = conClient.list_users(
-InstanceId='794790f5-0ae2-4348-806a-f58bf245ab3f',
+InstanceId=iid,
 MaxResults=10
 )
-users = response.get('UserSummaryList')
+
+while "NextToken" in response:
+    addUsers = response.get('UserSummaryList')
+
+    users.append(addUsers)
+    #print(response.get('UserSummaryList'))
+    response = conClient.list_users(
+            NextToken=response["NextToken"],
+            InstanceId=iid,
+            MaxResults=10)
+
+users.append(response.get('UserSummaryList'))
 print(type(users))
 print(users)
+
