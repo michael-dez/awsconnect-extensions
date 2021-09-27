75dd3117e6df6de3655ba6bd625716ea02e98823
mike <michael.dez@gmail.com>
2021-09-26 21:21:58 -0600

    before incorporating linked list

--- connect_scripts/add-extensions-all.py
+++ connect_scripts/add-extensions-all.py
@@ -1,27 +1,63 @@
 import boto3
 import json
-# TODO:function to check if agent has an extension, function to assign extension, and get_response function for exception handling and readability
+from boto3.dynamodb.conditions import Key,Attr
+# TODO:function to check if agent has an extension, function to assign extension, and check function for exception handling and readability
+ 
+# queries global secondary index for agent, if found returns true
+def has_extension(username):
+    u = username
+    dbClient = boto3.resource('dynamodb')
+    table = dbClient.Table('AgenttoAgent')
+    response = table.query(
+            IndexName='byAgent',
+            KeyConditionExpression=Key('AgentLoginName').eq(u))
+    hasExt = response.get('Items', {})
 
-conClient = boto3.client('connect')
-users = []
-iid ='794790f5-0ae2-4348-806a-f58bf245ab3f'
+    if (hasExt == []):
+        return False 
 
-response = conClient.list_users(
-InstanceId=iid,
-MaxResults=10
-)
+    else:
+        return True 
 
-while "NextToken" in response:
-    addUsers = response.get('UserSummaryList')
+def set_extension(username):
+    u = username
+    dbClient = boto3.resource('dynamodb')
+    table = dbClient.Table('AgenttoAgent')
+    
+    response1 = table.scan(
+    #TODO:scan parameters for available extensions
+    )
+    availExt = response1.get('Items', {})
+    
+    print (availExt)
 
-    users.append(addUsers)
-    #print(response.get('UserSummaryList'))
+
+    print(response1)
+def main():
+
+    conClient = boto3.client('connect')
+    users = []
+    iid ='794790f5-0ae2-4348-806a-f58bf245ab3f'
+
     response = conClient.list_users(
-            NextToken=response["NextToken"],
-            InstanceId=iid,
-            MaxResults=10)
+    InstanceId=iid,
+    MaxResults=10
+    )
 
-users.append(response.get('UserSummaryList'))
-print(type(users))
-print(users)
+    while "NextToken" in response:
+        addUsers = response.get('UserSummaryList')
+
+        users.append(addUsers)
+
+        response = conClient.list_users(
+                NextToken=response["NextToken"],
+                InstanceId=iid,
+                MaxResults=10)
+
+    users.append(response.get('UserSummaryList'))
+    set_extension('bbob') 
+    
+
+if __name__ == "__main__":
+    main()
 
