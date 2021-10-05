d27af637e9bbe85062bc6edd1e319999d0f70476
Michael Mendez <michael.dez@gmail.com>
2021-10-05 00:56:28 +0000

    left off at set extension function

--- connect_scripts/add-extensions-all.py
+++ connect_scripts/add-extensions-all.py
@@ -1,53 +1,35 @@
 import boto3
 import json
 from boto3.dynamodb.conditions import Key,Attr
+
 # TODO:function to check if agent has an extension, function to assign extension, and check function for exception handling and readability
  
-# queries global secondary index for agent, if found returns true
+dynamodb = boto3.resource('dynamodb', region_name = 'us-east-1') 
+table = dynamodb.Table('AgentData')
+
+connect = boto3.client('connect')
+
 global unused = []
 
+# checks if a user is currently assigned an extension 
 def has_extension(username):
     u = username
-    dbClient = boto3.resource('dynamodb')
-    table = dbClient.Table('AgenttoAgent')
+
     response = table.query(
             IndexName='byAgent',
             KeyConditionExpression=Key('AgentLoginName').eq(u))
     hasExt = response.get('Items', {})
 
-    if (hasExt == []):
+    if (bool(hasExt)):
         return False 
-
     else:
         return True 
-def get_unused_ext():
-
-    response = table.query(
-    IndexName='skIndex',
-    Limit=100,
-    KeyConditionExpression=Key('sk').eq("nu"))
-    
-    
-    unused = response.get('Items')
-
-    return unused
-
-def set_extension(username):
-    u = username
-    dbClient = boto3.resource('dynamodb')
-    table = dbClient.Table('AgenttoAgent')
-
-    if not bool(unused):
-        get_unused_ext()
-
-
-def main():
-
-    conClient = boto3.client('connect')
+        
+# gets list of connect users, returns as list
+def  get_users():
     users = []
     iid ='794790f5-0ae2-4348-806a-f58bf245ab3f'
-
-    response = conClient.list_users(
+    response = connect.list_users(
     InstanceId=iid,
     MaxResults=10
     )
@@ -57,15 +39,39 @@ def main():
 
         users.append(addUsers)
 
-        response = conClient.list_users(
+        response = connect.list_users(
                 NextToken=response["NextToken"],
                 InstanceId=iid,
                 MaxResults=10)
 
     users.append(response.get('UserSummaryList'))
-    set_extension('bbob') 
+    return users
+
+# gets unused extensions 100 at a time
+def get_unused_ext():
+    global unused
+
+    response = table.query(
+    IndexName='skIndex',
+    Limit=100,
+    KeyConditionExpression=Key('sk').eq("nu"))
     
+    
+    unused = response.get('Items')
+
+    return 
+#TODO: finish
+def set_extension(username):
+    u = username
+    global unused
+
+    if not bool(unused):
+        get_unused_ext()
+
+
+def main():
 
 if __name__ == "__main__":
     main()
+
 
